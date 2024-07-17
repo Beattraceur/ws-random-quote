a9e77aab801f37fb698467bb48c08444d2679388
Benjamin Hahl <contact@hahl.media>
2024-07-17 10:47:10 +0200

    layout adjustments for small screens

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,17 @@ export default function Home() {
   return (
     <main className='w-full flex flex-col items-center justify-between p-24'>
       {/*The centered main div with image and quote-component*/}
-      <div className='max-w-6xl flex flex-row gap-4'>
-        <div className='flex-1'>
+      <div className='max-w-6xl flex md:flex-row flex-col gap-4'>
+        <div className='flex-1 md:order-1'>
           {/*Next Image component with static image source*/}
           <Image
             src='/pictures/webdevNorris.jpg'
             alt='Chuck Norris as WebDev'
-            width={600}
-            height={600}
+            width={700}
+            height={700}
           />
         </div>
-        <div className='flex-1 flex items-center justify-center'>
+        <div className='flex-1 flex items-center justify-center md:order-2'>
           {/*Custom react Quote component*/}
           <Quote />
         </div>
